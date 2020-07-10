a4801156faf4590c69bfbabc9dfd45cb42ae7e0f
xuguozhong <dirsky@126.com>
2020-07-10 15:38:47 +0800

    增加ua中间件

--- ScrapyTutorial/middlewares.py
+++ ScrapyTutorial/middlewares.py
@@ -6,6 +6,7 @@
 # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
 from scrapy import signals
+from fake_useragent import UserAgent
 
 
 class ScrapytutorialSpiderMiddleware(object):
@@ -101,3 +102,8 @@ class ScrapytutorialDownloaderMiddleware(object):
 
     def spider_opened(self, spider):
         spider.logger.info('Spider opened: %s' % spider.name)
+
+
+class UserAgentDownloadMiddleware(object):
+    def process_request(self, request, spider):
+        request.headers.setdefault("User-Agent", UserAgent().random)
--- ScrapyTutorial/settings.py
+++ ScrapyTutorial/settings.py
@@ -52,9 +52,10 @@ COOKIES_ENABLED = False
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'ScrapyTutorial.middlewares.ScrapytutorialDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   # 'ScrapyTutorial.middlewares.ScrapytutorialDownloaderMiddleware': 543,
+   'ScrapyTutorial.middlewares.UserAgentDownloadMiddleware': 290,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
